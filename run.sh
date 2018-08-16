5f19fe5820058b0d56fab9ee79a30052b6bbad11
kolena <kolena@avast.com>
2018-08-16 15:25:47 +0200

    improved and enabled full set of input values

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-JAVA_OPTS="-Xmx4096m" bash -c "sbt jmh:run -i 5 -wi 5 -f1 -t1 \".*StreamingBenchmark.*\""
+JAVA_OPTS="-Xmx4096m" bash -c "sbt jmh:run -i 10 -wi 5 -f1 -t1 \".*StreamingBenchmark.*\""
--- src/main/scala/cz/jenda/benchmark/streaming/StreamingBenchmark.scala
+++ src/main/scala/cz/jenda/benchmark/streaming/StreamingBenchmark.scala
@@ -1,9 +1,9 @@
 package cz.jenda.benchmark.streaming
 
-import java.util.concurrent.{ArrayBlockingQueue, ForkJoinPool, ThreadPoolExecutor, TimeUnit}
+import java.util.concurrent.{ArrayBlockingQueue, ThreadPoolExecutor, TimeUnit}
 
-import fs2.Stream
 import cz.jenda.benchmark.streaming.StreamingBenchmark._
+import fs2.Stream
 import monix.eval.Task
 import monix.execution.Scheduler
 import monix.execution.schedulers.SchedulerService
@@ -19,41 +19,43 @@ import scala.concurrent.duration.Duration
 @Measurement(iterations = 5, time = 3, timeUnit = TimeUnit.SECONDS)
 @State(Scope.Benchmark)
 class StreamingBenchmark {
-  @Param(Array("2"))
-//  @Param(Array("2", "5", "10", "20", "50"))
+  @Param(Array("2", "5", "10", "20", "50"))
   var parallelism: Int = _
 
-  @Param(Array("10", "20"))
-//  @Param(Array("10", "50", "100", "500", "1000", "5000"))
+  @Param(Array("10", "50", "100", "500", "1000", "5000"))
   var items: Int = _
 
-  @Param(Array("100", "200"))
-//  @Param(Array("100", "500", "1000", "5000", "10000", "50000", "100000", "500000"))
+  @Param(Array("100", "500", "1000", "5000", "10000", "50000", "100000", "500000"))
   var size: Int = _
 
-  @Param(Array("10"))
-//  @Param(Array("5", "10", "20", "50", "100"))
+  @Param(Array("5", "10", "20", "50", "100"))
   var threads: Int = _
 
-  implicit var sch: SchedulerService = _
+  var schMonix: SchedulerService = _
+  var schFs2: SchedulerService = _
 
   @Setup
   def setup(): Unit = {
-    sch = Scheduler {
+    schMonix = Scheduler {
+      new ThreadPoolExecutor(threads, threads, 1, TimeUnit.MINUTES, new ArrayBlockingQueue[Runnable](1000))
+    }
+    schFs2 = Scheduler {
       new ThreadPoolExecutor(threads, threads, 1, TimeUnit.MINUTES, new ArrayBlockingQueue[Runnable](1000))
     }
   }
 
   @TearDown
   def tearDown(): Unit = {
-    sch.shutdown()
+    schMonix.shutdown()
   }
 
   @Benchmark
   def testMonix(): Unit = {
+    implicit val sch: SchedulerService = schMonix
+
     Observable
       .range(1, items)
-      .mapParallelUnordered(parallelism) { _ =>
+      .mapParallelUnordered[Vector[Byte]](parallelism) { _ =>
         Task {
           DataGenerator.get(size)
         }
@@ -66,6 +68,8 @@ class StreamingBenchmark {
 
   @Benchmark
   def testFs2(): Unit = {
+    implicit val sch: SchedulerService = schFs2
+
     Stream
       .range(1, items)
       .mapAsyncUnordered[Task, Vector[Byte]](parallelism) { _ =>
